0722abab021758c513c97f39bf8017d12c1dbfdd
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-11 21:27:45 -0500

    login and signup routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
-import Landing from '../Landing/Landing'
+import Landing from '../Landing/Landing';
+import Signup from '../Signup/Signup';
+import Login from '../Login/Login';
+
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={Landing} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </div>
   );
