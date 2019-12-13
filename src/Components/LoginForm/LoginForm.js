e2b3bb457925f23db525b12d731ad580a2763d66
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-12 20:09:20 -0500

    changed Navbar to static and create classes for login forms

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './LoginForm.css';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <form className='LoginForm'>
+                LoginForm
+            </form>
+        );
+    }
+}
+ 
+export default LoginForm;
