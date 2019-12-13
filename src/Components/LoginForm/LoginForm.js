efec772c5388dff3d39ecf0e67da1339b0f2a56a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-12 21:02:11 -0500

    Loginform wireframe complete

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
-import './LoginForm.css';
+import React, { Component } from "react";
+import "./LoginForm.css";
 
 class LoginForm extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {  
-            username: '',
-            password: '',
-            error: '',
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      error: ""
+    };
+  }
 
-    render() { 
-        return ( 
-            <form className='LoginForm'>
-                <fieldset>
-                    <section className='landing'>
-                        <div className='dark-overlay'>
-                            <div className='landing-inner'>
-                                <legend>
-                                    <h3>LOGIN</h3>
-                                </legend>
-                                
-                            </div>
-                        </div>
-                    </section>
-                </fieldset>
+  render() {
+    return (
+      <section className="landing LoginForm">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <form>
+              <fieldset>
+                <legend>
+                  <h3>LOGIN</h3>
+                </legend>
+                <div className="flex">
+                  <label htmlFor="username">
+                    <span className="input-title">* Username:</span>
+                    <input type="text" id="username" name="username" required />
+                  </label>
+                  <label htmlFor="password">
+                    <span className="input-title">* Password:</span>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      required
+                    />
+                  </label>
+                  <button className="btn btn-light" type="submit">
+                    Login
+                  </button>
+                </div>
+              </fieldset>
             </form>
-        );
-    }
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
- 
+
 export default LoginForm;
