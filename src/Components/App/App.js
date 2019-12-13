2140e78585647e3692c29cb5ad4393a146c16ffd
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-12 21:28:54 -0500

    completed wireframes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from '../Navbar/Navbar'
 import Landing from '../Landing/Landing';
-import Signup from '../Signup/Signup';
+import SignUpPage from '../../Routes/SignUpPage/SignUpPage';
 import LoginPage from '../../Routes/LoginPage/LoginPage';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Route path='/' component={Navbar} />
       <Switch>
         <Route exact path='/' component={Landing} />
-        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/signup' component={SignUpPage} />
         <Route exact path='/login' component={LoginPage} />
       </Switch>
     </div>
---
+++ src/Routes/SignUpPage/SignUpPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SignUpForm from '../../Components/SignupForm/SignupForm';
+
+class SignUpPage extends Component {
+    
+    render() { 
+        return (
+            <div className='SignUpPage'>
+                <SignUpForm />
+            </div>
+        );
+    }
+}
+ 
+export default SignUpPage;
