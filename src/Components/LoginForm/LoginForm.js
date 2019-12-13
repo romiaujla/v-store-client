a7cd77942fd309fbd1c4395b6ec71857eb4d0b24
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-12 20:39:51 -0500

    made changes to the login page and fixed navbar

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -16,32 +16,15 @@ class LoginForm extends Component {
         return ( 
             <form className='LoginForm'>
                 <fieldset>
-                    <legend>
-                        Login
-                    </legend>
-                    <section className='form-input-fields'>
-                        <label htmlFor='username'>
-                            <span>* Username</span>
-                            <input 
-                                className='form-input-text' 
-                                type='text'
-                                id='username'
-                                name='username'
-                                value={this.state.username}
-                                onChange={(e) => {this.setState({username: e.target.value})}}
-                            />
-                        </label>
-                        <label htmlFor='password'>
-                            <span>* Password</span>
-                            <input 
-                                className='form-input-text' 
-                                type='password'
-                                id='password'
-                                name='password'
-                                value={this.state.password}
-                                onChange={(e) => {this.setState({password: e.target.value})}}
-                            />
-                        </label>
+                    <section className='landing'>
+                        <div className='dark-overlay'>
+                            <div className='landing-inner'>
+                                <legend>
+                                    <h3>LOGIN</h3>
+                                </legend>
+                                
+                            </div>
+                        </div>
                     </section>
                 </fieldset>
             </form>
