18646f64d55b7aaf3126cff9d5ab6fe18d674b2e
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-14 11:27:26 -0500

    created a new component for new buyer profile page

---
+++ src/Components/NewBuyerProfile/NewBuyerProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Backdrop from '../Backdrop/Backdrop';
+
+class NewBuyerProfile extends Component {
+    
+    render() { 
+        return (
+            
+        );
+    }
+}
+
+
+export default NewBuyerProfile;
