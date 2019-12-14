06ba53c13b0db396bfbcdda835abccc6997dd0a3
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-14 11:30:11 -0500

    route to explore page

---
+++ src/Routes/ExplorePage/ExplorePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Backdrop from '../../Components/Backdrop/Backdrop';
+
+class ExplorePage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Backdrop />
+      </div>
+    );
+  }
+}
+
+export default ExplorePage;
