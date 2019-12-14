d89e922432d0511d3aa0a3a7d9fd6449c1273141
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-14 13:07:59 -0500

    added props to the card component

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
-import './Cards.css';
+import React, { Component } from "react";
+import "./Cards.css";
 
 export default class Cards extends Component {
+  
+  static defaultProps = {
+    shop_name: "Flip Flop USA",
+    service_type: "clothing and accessories",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  };
+
   render() {
     return (
-      <div className='card-body'>
-        <div className='card'>
-          <div className='card-image'></div>
-          <div className='card-text'>
-            <span className='date'>Open Since 2019</span>
-            <h2>Flip Flops USA</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <div className="card-body">
+        <div className="card">
+          <div className="card-image"></div>
+          <div className="card-text">
+            <span className="date">{this.props.shop_name}</span>
+            <h2>{this.props.service_type}</h2>
+            <p>{this.props.description}</p>
           </div>
         </div>
       </div>
--- src/HelperFunctions/Validation.js
+++ src/HelperFunctions/Validation.js
@@ -1,9 +1,17 @@
 const validUsername = (username) => {
     const usernameErrors = {
-        emptyString: 'Username is required, must be between 6 and 12 cahracters',
-        tooLong: ''
+        emptyString: 'Username is required, must be between 6 and 20 cahracters',
+        tooLong: 'Username cannot be longer than 20 characters',
+        spaces: 'Username cannot contain spaces',
     }
     if(!username.trim()){
-        
+        return usernameErrors.emptyString
     }
+    
+    const splitUsername = username.split(" ");
+    if(splitUsername.length > 1){
+        return usernameErrors.spaces
+    }
+
+    
 }
