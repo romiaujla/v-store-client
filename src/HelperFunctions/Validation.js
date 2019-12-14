10ae30b95fbcbfb4e702f859599f2e91fbd4b3b7
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-14 11:27:54 -0500

    created a file to have validaiton functions

---
+++ src/HelperFunctions/Validation.js
@@ -0,0 +1,9 @@
+const validUsername = (username) => {
+    const usernameErrors = {
+        emptyString: 'Username is required, must be between 6 and 12 cahracters',
+        tooLong: ''
+    }
+    if(!username.trim()){
+        
+    }
+}
