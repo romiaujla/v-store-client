bc90a73ea8dd1dc09966b0d0b7a2badae19ecfdd
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-14 11:27:41 -0500

    created state to update the input fields

--- src/Components/SignupForm/SignupForm.js
+++ src/Components/SignupForm/SignupForm.js
@@ -3,8 +3,25 @@ import './SignupForm.css';
 import Backdrop from '../Backdrop/Backdrop';
 
 export default class Signup extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      error: {
+        username: false,
+        password: false,
+        usernameError: 'Invalid Username',
+        passwordError: 'Invalid Password',
+      },
+    }
+  }
   
   render() {
+
+    const {error} = this.state;
+
     return (
       <form>
           <fieldset>
@@ -14,7 +31,18 @@ export default class Signup extends Component {
             <div className="flex">
               <label htmlFor="username">
                 <span className="input-title">* Username:</span>
-                <input type="text" id="username" name="username" required />
+                <input 
+                  type="text" 
+                  id="username" 
+                  name="username" 
+                  required 
+                  value={this.state.username}
+                  onChange = {(e) => {this.setState({username : e.target.value})}}
+                />
+                {
+                  error.username && 
+                    <span className='error'>{error.usernameError}</span>
+                }
               </label>
               <label htmlFor="password">
                 <span className="input-title">* Password:</span>
@@ -23,7 +51,13 @@ export default class Signup extends Component {
                   id="password"
                   name="password"
                   required
+                  value={this.state.password}
+                  onChange = {(e) => {this.setState({password : e.target.value})}}
                 />
+                {
+                  error.password &&
+                    <span className='error'>{error.passwordError}</span>
+                }
               </label>
               <label htmlFor="password">
                 <span className="input-title">* Select Account Type</span>
