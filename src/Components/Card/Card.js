1255dfe6286e59ad737d43ffa9c83c00f6aa8f97
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2019-12-21 12:27:37 -0500

    character limit

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -16,6 +16,13 @@ export default class Cards extends Component {
     this.props.handleShopCardClick(this.props.shop_id);
   }
 
+  limitDescription = (description, length) => {
+    if (description.length <= length) {
+      return description
+    }
+    return description.slice(0, length) + '...'
+  }
+
   render() {
     return (
       <div className="card-container" onClick={() => { this.handleShopCardClick() }}>
@@ -29,7 +36,7 @@ export default class Cards extends Component {
           <div className="card-text">
             <span className='store-type'>{this.props.service_type}</span>
             <h2>{this.props.shop_name}</h2>
-            <p>{this.props.description}</p>
+            <p>{this.limitDescription(this.props.description, 100)}</p>
           </div>
          
       </div>
