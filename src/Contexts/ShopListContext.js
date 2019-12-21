ec21c89c78fdcb063723baa1960f25b2eacab814
Chau Tran <cmtran7393@gmail.com>
2019-12-21 14:08:25 -0500

    changed name for Shops Context

---
+++ src/Contexts/ShopListContext.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const ShopListContext = React.createContext({
+    shops: [],
+    error: null,
+    setShops: () => {},
+})
+
+export default ShopListContext;
+
+export class ShopListProvider extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            shops: [],
+            error: null,
+        }
+    }
+
+    // set the shops in the state
+    setShops = (shops) => {
+        this.setState({
+            shops,
+        })
+    }
+
+
+    render() { 
+
+        const obj = {
+            shops: this.state.shops,
+            error: this.state.error,
+            setShops: this.setShops,
+        }
+
+        return (
+            <ShopListContext.Provider value={obj}>
+                {this.props.children}
+            </ShopListContext.Provider>
+        );
+    }
+}
