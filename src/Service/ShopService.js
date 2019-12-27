0253cfe48eec7a84ad44dd0c371bee6086d3d62e
Chau Tran <cmtran7393@gmail.com>
2019-12-27 11:55:59 -0500

    added products endpoint

--- src/Service/ShopService.js
+++ src/Service/ShopService.js
@@ -27,8 +27,24 @@ const ShopService = {
         })
             .then(res => res.json())
             .then(shop => {
-                console.log(shop);
+                // console.log(shop);
                 return shop;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },
+    getShopProducts(shopId){
+        return fetch(`${config.API_ENDPOINT}/shops/${shopId}/products`, {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(products => {
+                // console.log('products', products);
+                return products;
             })
             .catch(err => {
                 console.log(err);
