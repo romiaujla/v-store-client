aa33c55c9f2fa479c9f86542dedb638b2361256d
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-29 10:37:38 -0500

    added functions to get shops by id or category

--- src/Contexts/ShopListContext.js
+++ src/Contexts/ShopListContext.js
@@ -4,6 +4,9 @@ const ShopListContext = React.createContext({
     shops: [],
     error: null,
     setShops: () => {},
+    getShops: () => {},
+    getShopById: () => {},
+    getShopsByCategory: () => {},
 })
 
 export default ShopListContext;
@@ -24,14 +27,33 @@ export class ShopListProvider extends Component {
             shops,
         })
     }
+
     setError = (error) =>{
         this.setState({
             error,
         })
     }
+
     clearError = () =>{
         this.setState({error: null})
     }
+
+    getShopById = (id) => {
+        // == only so that even if id is a string it will match and send the result
+        return this.state.shops.filter(shop => shop.id == id)
+    }
+
+    setShopById = (shop, id) => {
+        // change the shop with the provided id
+    }
+
+    getShops = () => {
+        return this.state.shops;
+    }
+
+    getShopsByCategory = (service_type) => {
+        return this.state.shops.filter(shop => shop.service_type === service_type);
+    } 
 
     render() { 
 
@@ -39,6 +61,9 @@ export class ShopListProvider extends Component {
             shops: this.state.shops,
             error: this.state.error,
             setShops: this.setShops,
+            getShops: this.getShops,
+            getShopById: this.getShopById,
+            getShopsByCategory: this.getShopsByCategory,
         }
 
         return (
