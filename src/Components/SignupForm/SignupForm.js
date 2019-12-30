9edb605944cef4100c71de0766dedfbb0c0746d5
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-30 12:52:50 -0500

    still work in progress

--- src/Components/SignupForm/SignupForm.js
+++ src/Components/SignupForm/SignupForm.js
@@ -8,21 +8,36 @@ export default class Signup extends Component {
     this.state = {
       username: '',
       password: '',
-      error: {
-        username: false,
-        password: false,
-        usernameError: 'Invalid Username',
-        passwordError: 'Invalid Password',
-      },
+      user_type: -1,
+      error: true,
+      usernameLengthError: true,
+      passwordLengthError: true,
     }
+  }
+
+  handleRegisteration = (e) => {
+    e.preventDefault();
+    const user = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+      user_type: e.target.user_type.value,
+    }
+    console.log(user);
   }
   
   render() {
 
     const {error} = this.state;
+    const redColor = {
+      color: 'red',
+    }
+    const greenColor = {
+      color: 'green',
+    }
+
 
     return (
-      <form>
+      <form className='SignUpForm' onSubmit={(e) => {this.handleRegisteration(e)}}>
           <fieldset>
             <legend>
               <h3>Register</h3>
@@ -36,12 +51,10 @@ export default class Signup extends Component {
                   name="username" 
                   required 
                   value={this.state.username}
-                  onChange = {(e) => {this.setState({username : e.target.value})}}
+                  onChange = {(e) => {this.handleUserNameChange(e.target.value)}}
                 />
-                {
-                  error.username && 
-                    <span className='error'>{error.usernameError}</span>
-                }
+                <span className='error' style={redColor}>Username cannot be empty</span>
+                <span className='error' style={redColor}>Username must be 6 to 72 characters long</span>
               </label>
               <label htmlFor="password">
                 <span className="input-title">* Password:</span>
@@ -51,22 +64,36 @@ export default class Signup extends Component {
                   name="password"
                   required
                   value={this.state.password}
-                  onChange = {(e) => {this.setState({password : e.target.value})}}
+                  onChange = {(e) => {this.handlePasswordChange(e.target.value)}}
                 />
                 {
                   error.password &&
                     <span className='error'>{error.passwordError}</span>
                 }
               </label>
-              <label htmlFor="password">
+              <label htmlFor="user_type">
                 <span className="input-title">* Select Account Type</span>
-                <select className='select-css'>
+                <select 
+                  className='select-css'
+                  name='user_type'
+                  id='user_type'
+                  defaultValue={this.state.user_type}
+                  onChange={(e) => {this.handleUserTypeChange(e.target.value)}}
+                >
                   <option value='-1'>Select One Type</option>
-                  <option value='1'>Seller</option>
-                  <option value='2'>Buyer</option>
+                  <option value='shop'>Seller</option>
+                  <option value='buyer'>Buyer</option>
                 </select>
+                {
+                  error.user_type &&
+                    <span className='error'>{error.user_typeError}</span>
+                }
               </label>
-              <button className="btn btn-light" type="submit">
+              <button 
+                className="btn btn-light" 
+                type="submit"
+                disabled={this.state.disableButton}
+              >
                 Register
               </button>
             </div>
