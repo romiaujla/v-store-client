2b739d1e4dbc17b320f189c3e29c586cf88a5c7a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-12-31 19:28:20 -0500

    imported style sheet

--- src/Routes/SignUpPage/SignUpPage.js
+++ src/Routes/SignUpPage/SignUpPage.js
@@ -4,18 +4,17 @@ import SignUpForm from '../../Components/SignUpForm/SignUpForm';
 import AuthService from '../../Service/AuthService';
 import TokenService from '../../Service/TokenService';
 import LoggedContext from '../../Contexts/LoggedContext';
+import './SignUpPage.css';
 
 class SignUpPage extends Component {
 
     static contextType = LoggedContext;
 
     handleRegistration = async (username, password) => {
-        console.log(username, password);
         await AuthService.postLogin({
             username,
             password,
           }).then(res => {
-              console.log(res.userType, res.userTypeId);
               TokenService.saveAuthToken(res.authToken);
               this.context.setUserType(res.userType, res.userTypeId);
           }).catch((err) => {
