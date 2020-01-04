2865694efede286adad9d5f97b8fce2c961caaa7
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-04 14:29:54 -0500

    Updates

---
+++ precache-manifest.b8b2fc06dcb67e3636f5973266a20524.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1c6780119fd7b192526045280796ae",
+    "url": "/romiaujla/shopzilla-client/index.html"
+  },
+  {
+    "revision": "c15cab53c3beee40aec4",
+    "url": "/romiaujla/shopzilla-client/static/css/2.3bc790af.chunk.css"
+  },
+  {
+    "revision": "b52e72a1a6137bf2c2d0",
+    "url": "/romiaujla/shopzilla-client/static/css/main.fc822fe5.chunk.css"
+  },
+  {
+    "revision": "c15cab53c3beee40aec4",
+    "url": "/romiaujla/shopzilla-client/static/js/2.bc5554ca.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/romiaujla/shopzilla-client/static/js/2.bc5554ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b52e72a1a6137bf2c2d0",
+    "url": "/romiaujla/shopzilla-client/static/js/main.3a35285d.chunk.js"
+  },
+  {
+    "revision": "71c1a4961414254f8592",
+    "url": "/romiaujla/shopzilla-client/static/js/runtime-main.0d3a0547.js"
+  },
+  {
+    "revision": "9ea689c7b6b8cf22b538d576db125a00",
+    "url": "/romiaujla/shopzilla-client/static/media/braceletsoflife.9ea689c7.png"
+  },
+  {
+    "revision": "679f6d844b32dba062b3ff4813532cca",
+    "url": "/romiaujla/shopzilla-client/static/media/burger1.679f6d84.png"
+  },
+  {
+    "revision": "90ba897ef675fae50816203e04cc589c",
+    "url": "/romiaujla/shopzilla-client/static/media/burger2.90ba897e.png"
+  },
+  {
+    "revision": "cbbbaa15aff3426c7dbef89b0db3c5b4",
+    "url": "/romiaujla/shopzilla-client/static/media/burger3.cbbbaa15.png"
+  },
+  {
+    "revision": "cc05218b14186b336cbd3b854d40ac18",
+    "url": "/romiaujla/shopzilla-client/static/media/burger4.cc05218b.png"
+  },
+  {
+    "revision": "63296490338d0614d230ca595b87ab4a",
+    "url": "/romiaujla/shopzilla-client/static/media/burger5.63296490.png"
+  },
+  {
+    "revision": "fb3c23c967b5fd55ac9924ec6de13d42",
+    "url": "/romiaujla/shopzilla-client/static/media/burgerlicious.fb3c23c9.png"
+  },
+  {
+    "revision": "5b0c41f8866a047a4a655c91d5887b83",
+    "url": "/romiaujla/shopzilla-client/static/media/coffee-1.5b0c41f8.png"
+  },
+  {
+    "revision": "ecb443e2d17229002d6abfa7d381c5c6",
+    "url": "/romiaujla/shopzilla-client/static/media/coffee-2.ecb443e2.png"
+  },
+  {
+    "revision": "5f532bd888f8908333c9bc4477128bbc",
+    "url": "/romiaujla/shopzilla-client/static/media/customboards.5f532bd8.png"
+  },
+  {
+    "revision": "26008e139337b46125d2bed20b86de9c",
+    "url": "/romiaujla/shopzilla-client/static/media/default-products.26008e13.jpg"
+  },
+  {
+    "revision": "c512655ddbee785adbcd1a70f7a43e47",
+    "url": "/romiaujla/shopzilla-client/static/media/flipflops.c512655d.png"
+  },
+  {
+    "revision": "28089a071dede4565369a8e520cd4a9a",
+    "url": "/romiaujla/shopzilla-client/static/media/logo-white.28089a07.png"
+  },
+  {
+    "revision": "6ee6f32348fb4e12918f67f161a32be7",
+    "url": "/romiaujla/shopzilla-client/static/media/nightmarket.6ee6f323.jpg"
+  },
+  {
+    "revision": "f54c5250770eac7d9311d6cf196d82a9",
+    "url": "/romiaujla/shopzilla-client/static/media/product.f54c5250.png"
+  },
+  {
+    "revision": "74f536b74296fdc1143583d5c4d83ee7",
+    "url": "/romiaujla/shopzilla-client/static/media/shop.74f536b7.png"
+  },
+  {
+    "revision": "8b1b151f6154fa1e3ea92eec1eda61d3",
+    "url": "/romiaujla/shopzilla-client/static/media/skateboard1.8b1b151f.png"
+  },
+  {
+    "revision": "bfcb4e406746af06e3d74f607c2433a7",
+    "url": "/romiaujla/shopzilla-client/static/media/skateboard2.bfcb4e40.png"
+  },
+  {
+    "revision": "35ec424b80a61aa4d37d755143b9a394",
+    "url": "/romiaujla/shopzilla-client/static/media/skateboard3.35ec424b.png"
+  },
+  {
+    "revision": "00dd8d046a85cf78d290ec08b54f6fd9",
+    "url": "/romiaujla/shopzilla-client/static/media/skateboard4.00dd8d04.png"
+  },
+  {
+    "revision": "6d7fb1b6b164ba444b221af675a1735d",
+    "url": "/romiaujla/shopzilla-client/static/media/skateboard5.6d7fb1b6.png"
+  },
+  {
+    "revision": "255e3f34d17915aa73b1d2a08a554be7",
+    "url": "/romiaujla/shopzilla-client/static/media/soap1.255e3f34.png"
+  },
+  {
+    "revision": "06738d6e84854b397788e26a776569d6",
+    "url": "/romiaujla/shopzilla-client/static/media/soap2.06738d6e.png"
+  },
+  {
+    "revision": "c56472aa0401c7127a6ae3801b0d5d7c",
+    "url": "/romiaujla/shopzilla-client/static/media/soap3.c56472aa.png"
+  },
+  {
+    "revision": "325bf5c218d0e78e5e689fb33c89b56d",
+    "url": "/romiaujla/shopzilla-client/static/media/soap4.325bf5c2.png"
+  },
+  {
+    "revision": "ab382f1e6f7231b135bfdafcb5c46855",
+    "url": "/romiaujla/shopzilla-client/static/media/soap5.ab382f1e.png"
+  },
+  {
+    "revision": "f8e0ecd95bbe426299a925498ca4712c",
+    "url": "/romiaujla/shopzilla-client/static/media/thesoapshop.f8e0ecd9.png"
+  },
+  {
+    "revision": "2f0e394f044713a485898ae9510add55",
+    "url": "/romiaujla/shopzilla-client/static/media/thetoybarn.2f0e394f.png"
+  },
+  {
+    "revision": "0810cb85691a2fa100b5706da3fbf745",
+    "url": "/romiaujla/shopzilla-client/static/media/toy1.0810cb85.png"
+  },
+  {
+    "revision": "42499134d0895e6d97c82c7ea626c528",
+    "url": "/romiaujla/shopzilla-client/static/media/toy2.42499134.png"
+  },
+  {
+    "revision": "0a1dd33554f03afa669369134b11479d",
+    "url": "/romiaujla/shopzilla-client/static/media/toy3.0a1dd335.png"
+  },
+  {
+    "revision": "ad66ba1fab273d1090148e45f9f1033e",
+    "url": "/romiaujla/shopzilla-client/static/media/toy4.ad66ba1f.png"
+  },
+  {
+    "revision": "4293818e8ca34016197bddaec3b3812f",
+    "url": "/romiaujla/shopzilla-client/static/media/toy5.4293818e.png"
+  },
+  {
+    "revision": "572b3a2f7045be92b75ca0a5753f8a04",
+    "url": "/romiaujla/shopzilla-client/static/media/tully.572b3a2f.png"
+  }
+]);
