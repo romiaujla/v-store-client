16f1575f7dca1bcf47892525bc77e5e052620dbc
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-04 16:12:57 -0500

    changed API_ENDPOINT to user heroku server path

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 const path = 'https://intense-castle-13181.herokuapp.com/api';
 
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || path,
+    API_ENDPOINT: path,
     TOKEN_KEY: process.env.TOKEN_KEY || '9e7d73be-f7f1-11e9-8f0b-362b9e155667',
     BASEPATH: '/shopzilla-client',
 }
